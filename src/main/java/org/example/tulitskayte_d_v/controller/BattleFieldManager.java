14f73a834746bf8bb07ee2183d75376f7dd8699c
sinnmora <jonshilin.gg@gmail.com>
2024-01-04 20:55:28 +0300

    added IBattleFieldManager interface for further implementation

--- src/main/java/org/example/tulitskayte_d_v/controller/BattleFieldManager.java
+++ src/main/java/org/example/tulitskayte_d_v/controller/BattleFieldManager.java
@@ -10,44 +10,5 @@ import org.example.tulitskayte_d_v.model.ships.ShipStates;
 
 import java.util.List;
 
-public class BattleFieldManager {
-
-    private final BattleField battleField;
-
-    public BattleFieldManager(int size, List<Ship> ships) {
-        this.battleField = new BattleField(size, ships);
-    }
-
-    public HitResults hitBattleField(Coordinate coordinate) {
-        if (!battleField.isValidMove(coordinate)) {
-            throw new IllegalStateException("Invalid move");
-        }
-        return battleField.hitBattleField(coordinate);
-    }
-
-    public void arrangeShips(List<Ship> ships) {
-        battleField.arrangeTheShips(ships);
-    }
-
-    public Cell[][] getCells() {
-        return battleField.copyCells();
-    }
-
-    public boolean isEnemyLose() {
-        return battleField.isEnemyLose();
-    }
-
-    public int getSize() {
-        return battleField.getSize();
-    }
-
-    public List<Ship> getShips() {
-        return battleField.getShips();
-    }
-    public ShipDeck getShip(int row, int column) {
-        return battleField.getShip(row, column);
-    }
-    public boolean isValidMove(Coordinate coordinate) {
-        return battleField.isValidMove(coordinate);
-    }
+public class BattleFieldManager implements IBattleFieldManager {
 }
---
+++ src/main/java/org/example/tulitskayte_d_v/controller/IBattleFieldManager.java
@@ -0,0 +1,4 @@
+package org.example.tulitskayte_d_v.controller;
+
+public interface IBattleFieldManager {
+}
